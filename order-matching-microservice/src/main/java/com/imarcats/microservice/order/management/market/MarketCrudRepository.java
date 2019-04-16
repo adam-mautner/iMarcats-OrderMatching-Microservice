8d0a4c313dee2ab98d18aa5c2522fe68d9495b47
Adam Mautner <adam.mautner@abiscon.com>
2019-04-16 22:31:36 +0200

    Deleting unnecessary files

--- order-matching-microservice/src/main/java/com/imarcats/microservice/order/management/market/MarketCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.imarcats.microservice.order.management.market;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.imarcats.model.Market;
-
-public interface MarketCrudRepository extends CrudRepository<Market, String> {
-
-}
